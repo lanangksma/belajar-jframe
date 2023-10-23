649e1810e93bf5b11dcebfc4fa735b847fcfc143
lanangksma <kusumalanang1@gmail.com>
2023-10-24 01:11:43 +0700

    Message Dialog

--- Pertemuan-6/src/HelloMessageDialog.java
+++ Pertemuan-6/src/HelloMessageDialog.java
@@ -1,35 +1,42 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*; // Import semua class dari package javax.swing
+import java.awt.*; // Import semua class dari package java.awt
+import java.awt.event.ActionEvent; // Import class ActionEvent dari package java.awt.event
+import java.awt.event.ActionListener; // Import class ActionListener dari package java.awt.event
 
+// Membuat class HelloMessageDialog yang merupakan turunan dari class JFrame
 public class HelloMessageDialog extends JFrame {
 
+    // Membuat constructor HelloMessageDialog
     public HelloMessageDialog() {
+        // Menutup aplikasi saat mengklik tombol close
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Mengatur layout frame utama
         this.setLayout(new FlowLayout());
 
+        // Membuat tombol "Klik!" dan menentukan posisi dan ukurannya
         JButton button = new JButton("Klik!");
         button.setBounds(130, 100, 100, 40);
 
-//        button.addActionListener(e -> {
-//            JOptionPane.showMessageDialog(HelloMessageDialog.this, "Halo, selamat datang di praktikum pemograman II");
-//        });
-
+        // Menambahkan action listener untuk tombol "Klik!" dengan JOptionPane sebagai actionnya
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Menampilkan pesan dialog
                 JOptionPane.showMessageDialog(HelloMessageDialog.this, "Halo, selamat datang di praktikum pemograman II");
             }
         });
 
+        // Menambahkan tombol "Klik!" ke frame utama dan menentukan ukuran frame utama
         this.add(button);
         this.setSize(200, 200);
     }
 
+    // Membuat method main untuk menjalankan aplikasi GUI
     public static void main(String[] args) {
+        // Menjalankan aplikasi GUI dengan menggunakan thread yang berjalan di EDT (Event Dispatch Thread)
         javax.swing.SwingUtilities.invokeLater(() -> {
+            // Membuat objek HelloMessageDialog dan menampilkannya
             HelloMessageDialog h = new HelloMessageDialog();
             h.setVisible(true);
         });
--- Pertemuan-6/src/HelloShowMessageDialog.java
+++ Pertemuan-6/src/HelloShowMessageDialog.java
@@ -1,20 +1,27 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*; // Import semua class dari package javax.swing
+import java.awt.*; // Import semua class dari package java.awt
+import java.awt.event.ActionEvent; // Import class ActionEvent dari package java.awt.event
+import java.awt.event.ActionListener; // Import class ActionListener dari package java.awt.event
 
+// Membuat class HelloShowMessageDialog yang merupakan turunan dari class JFrame
 public class HelloShowMessageDialog extends JFrame {
 
+    // Membuat constructor HelloShowMessageDialog
     public HelloShowMessageDialog() {
+        // Menutup aplikasi saat mengklik tombol close
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Membuat tombol-tombol untuk menampilkan pesan dialog
         JButton error = new JButton("Show Message Dialog ERROR");
         JButton infromation = new JButton("Show Message Dialog INFORMATION");
         JButton warning = new JButton("Show Message Dialog WARNING");
         JButton question = new JButton("Show Message Dialog QUESTION");
         JButton plain = new JButton("Show Message Dialog PLAIN");
+
+        // Mengatur layout frame utama menjadi FlowLayout
         this.setLayout(new FlowLayout());
 
+        // Menambahkan action listener untuk tombol error dengan JOptionPane ERROR_MESSAGE sebagai actionnya
         error.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -22,6 +29,7 @@ public class HelloShowMessageDialog extends JFrame {
             }
         });
 
+        // Menambahkan action listener untuk tombol infromation dengan JOptionPane INFORMATION_MESSAGE sebagai actionnya
         infromation.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -29,6 +37,7 @@ public class HelloShowMessageDialog extends JFrame {
             }
         });
 
+        // Menambahkan action listener untuk tombol warning dengan JOptionPane WARNING_MESSAGE sebagai actionnya
         warning.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,6 +45,7 @@ public class HelloShowMessageDialog extends JFrame {
             }
         });
 
+        // Menambahkan action listener untuk tombol question dengan JOptionPane QUESTION_MESSAGE sebagai actionnya
         question.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -43,23 +53,29 @@ public class HelloShowMessageDialog extends JFrame {
             }
         });
 
+        // Menambahkan action listener untuk tombol plain dengan JOptionPane PLAIN_MESSAGE sebagai actionnya
         plain.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(HelloShowMessageDialog.this, "Halo, selamat datang di praktikum pemograman II", "PLAIN", JOptionPane.PLAIN_MESSAGE);
             }
         });
+        // NOTE : HelloShowMessageDialog.this merupakan parent component dari JOptionPane yang akan ditampilkan di tengah-tengah frame utama (HelloShowMessageDialog)
 
+        // Menambahkan tombol-tombol ke frame utama dan menentukan ukuran frame utama
         this.add(error);
         this.add(infromation);
         this.add(warning);
         this.add(question);
         this.add(plain);
 
+        // Menentukan ukuran frame utama menjadi 400x400
         this.setSize(400, 400);
     }
 
+    // Membuat method main untuk menjalankan aplikasi GUI
     public static void main(String[] args) {
+        // Menjalankan aplikasi GUI dengan menggunakan thread yang berjalan di EDT (Event Dispatch Thread)
         javax.swing.SwingUtilities.invokeLater(() -> {
             HelloShowMessageDialog h = new HelloShowMessageDialog();
             h.setVisible(true);
